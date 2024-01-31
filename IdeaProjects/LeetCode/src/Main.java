dad6ca026a5c26bc08c18a6973a85964171960ed
Supriya Narala <supriyanarala9@gmail.com>
2024-01-31 11:45:17 -0600

    1st commit for leetcode

---
+++ IdeaProjects/LeetCode/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args)
+    {
+
+                Set<Short> s = new HashSet<>();
+                for (short i = 0; i < 100; i++) {
+                    s.add(i);
+                   // s.remove(i - 1);
+                    System.out.println(s);
+                }
+                System.out.println(s.size());
+
+    }
+}
