f8f2b62b368454388bff6f381013947ee607b80e
supriyanarala <157667513+supriyanarala@users.noreply.github.com>
2024-02-02 14:12:39 -0600

    Delete Desktop/Day1Project_JavaIntro directory

--- Desktop/Day1Project_JavaIntro/src/Review.java
+++
@@ -1,8 +0,0 @@
-public class Review{
-    public static void main(String[] args){
-//The main method executes the tasks of the class
-        System.out.println("My first Java program from scratch!.");
-        //for commiting to git
-    }
-}
-
